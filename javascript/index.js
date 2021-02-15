c8856f6c11b9667eab0a8987aa0b51a121aaec27
satoshi-takamatsu <70362048+satoshi-takamatsu@users.noreply.github.com>
2021-02-15 19:11:23 +0900

    入力したテキストを表示させるところまで戻る

--- javascript/index.js
+++ javascript/index.js
@@ -1,59 +1,88 @@
-window.addEventListener('load', function(){
-  const button1 = document.getElementById('button1');
+// window.addEventListener('load', function(){
+//   const button1 = document.getElementById('button1');
 
-  button1.addEventListener('mouseover', function(){
-    document.getElementById('button1').style.background='blue';
-    document.getElementById('button1').style.color='white';
-  });
-  button1.addEventListener('mouseout', function(){
-    document.getElementById('button1').style.background='white';
-    document.getElementById('button1').style.color='black';
-  });
-});
+//   button1.addEventListener('mouseover', function(){
+//     document.getElementById('button1').style.background='blue';
+//     document.getElementById('button1').style.color='white';
+//   });
+//   button1.addEventListener('mouseout', function(){
+//     document.getElementById('button1').style.background='white';
+//     document.getElementById('button1').style.color='black';
+//   });
+// });
 
-document.addEventListener('DOMContentLoaded', function(){
-  const addTaskTrigger = document.getElementsByClassName('addTask-trigger')[0];
-  const addTaskValue = document.getElementsByClassName('addTask-value')[0];
-  const addTaskTarget = document.getElementsByClassName('addTask-target')[0];
+// document.addEventListener('DOMContentLoaded', function(){
+//   const TaskTrigger = document.getElementsByClassName('addTask-trigger')[0];
+//   const TaskValue = document.getElementsByClassName('addTask-value')[0];
+//   const TaskTarget = document.getElementsByClassName('addTask-target')[0];
 
-  let nextId = 0;
-  const addTask = (task,id) => {
+//   let nextId = 0;
+//   const addTask = (task,id) => {
+//   const todos = [];
+
+//     const tableItem = document.createElement('tr');
+//     const TdButton = document.createElement('td');
+//     const Button = document.createElement('button');
+//     const backTdButton = document.createElement('td');
+//     const backButton = document.createElement('button');
     
-    const tableItem = document.createElement('tr');
-    const addButtonTd = document.createElement('td');
-    const addButton = document.createElement('button');
-    const removeButtonTd = document.createElement('td');
-    const removeButton = document.createElement('button');
+//     Button.innerText = '作業中';
+//     backButton.innerText = '削除';
     
-    addButton.innerText = '作業中';
-    removeButton.innerText = '削除';
+//     backButton.addEventListener('click', () => removeTask(backButton));
     
-    removeButton.addEventListener('click', () => removeTask(removeButton));
+//     const idSpan = document.createElement('span');
+//     idSpan.innerText = id;
+//     const idSpanTd = document.createElement('td');
+//     idSpanTd.append(idSpan);
     
-    const idSpan = document.createElement('span');
-    idSpan.innerText = id;
-    const idSpanTd = document.createElement('td');
-    idSpanTd.append(idSpan);
+//     const taskSpan = document.createElement('span');
+//     const taskSpanTd = document.createElement('td');
+//     taskSpan.innerText = task;
+//     taskSpanTd.append(taskSpan);
     
-    const taskSpan = document.createElement('span');
-    const taskSpanTd = document.createElement('td');
-    taskSpan.innerText = task;
-    taskSpanTd.append(taskSpan);
+//     TdButton.append(Button);
+//     backTdButton.append(backButton);
     
-    addButtonTd.append(addButton);
-    removeButtonTd.append(removeButton);
+//     tableItem.append(idSpanTd);
+//     tableItem.append(taskSpanTd);
+//     tableItem.append(TdButton);
+//     tableItem.append(backTdButton);
     
-    tableItem.append(idSpanTd);
-    tableItem.append(taskSpanTd);
-    tableItem.append(addButtonTd);
-    tableItem.append(removeButtonTd);
-    
-    addTaskTarget.appendChild(tableItem);
-  };
+//     TaskTarget.appendChild(tableItem);
+//   };
+
   
-  addTaskTrigger.addEventListener('click', event => {
-    const task = addTaskValue.value;
-    addTask(task,nextId ++);
-    addTaskValue.value =  '';
+//   TaskTrigger.addEventListener('click', event => {
+//     const task = TaskValue.value;
+//     addTask(task,nextId ++);
+//     TaskValue.value =  '';
+//   });
+//   TaskTrigger.addEventListener('click', event => {
+//     const task = TaskValue.value;
+//     const status = idSpan.id;
+//   });
+
+//   todos.push(todo);
+// });
+
+
+
+  // document.querySelector('button').addEventListener('click', () => {
+  //   const li = document.createElement('li'); 
+  //   const text = document.querySelector('input');
+  //   li.textContent = text.value;
+  //   document.querySelector('ul').appendChild(li);
+
+  //   text.value = '';
+  //   text.focus();
+  // });
+  document.querySelector('button').addEventListener('click', () => {
+    const li = document.createElement('li'); 
+    const text = document.getElementById('todoText');
+    li.textContent = text.value;
+    document.querySelector('ul').appendChild(li);
+
+    text.value = '';
+    text.focus();
   });
-});
